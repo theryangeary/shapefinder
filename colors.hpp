bc3a4f8ef91f2e4a49467585548b0adf469cb013
Ryan Geary <rtgnj42@gmail.com>
2018-10-29 14:14:16 -0400

    Add white to color list

--- colors.hpp
+++ colors.hpp
@@ -2,13 +2,15 @@
 #include "opencv2/core/core.hpp"
 #include <map>
 
-#define RED   Scalar(255,0,0)
+#define BLUE   Scalar(255,0,0)
 #define GREEN Scalar(0,255,0)
-#define BLUE  Scalar(0,0,255)
+#define RED  Scalar(0,0,255)
+#define WHITE Scalar(255,255,255)
 
 std::map<std::string, cv::Scalar> colorMap = {
   {"red", cv::RED},
   {"green", cv::GREEN},
-  {"blue", cv::BLUE}
+  {"blue", cv::BLUE},
+  {"white", cv::WHITE}
 };
 
