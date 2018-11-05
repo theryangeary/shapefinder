5cadec35a61279752c9d9ea517fcb396e4cd37db
Ryan Geary <rtgnj42@gmail.com>
2018-11-04 22:14:14 -0500

    Add usage statement

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include "shapefinder.hpp"
 
+void usage() {
+  cout << "Usage: ";
+  cout << "./main <image>" << endl;
+  cout << "\tOptions:" << endl;
+  cout << "\t-c, --color\tColor to outline found shapes with. One of red, blue, green, or a hex color such as \"#74fed9\". If no color is specified, red will be used." << endl;
+  cout << "\t-o, --output\tPath to save output to. If no extension is specified then .png will be used." << endl;
+  cout << "\t-s, --shape\tShape to find. One of rectangle or triangle. If not shape is specified, rectangle will be used." << endl;
+}
+
 int main(int argc, char** argv) {
   string inputFile = "";
   string outfile = "./images/output.png";
@@ -27,6 +36,13 @@ int main(int argc, char** argv) {
   cout << "Output: " << outfile << endl;
   cout << "Shape: " << shape << endl;
   cout << "Color: " << color << endl;
-  entry(inputFile, outfile, color, shape);
+  try {
+    if (entry(inputFile, outfile, color, shape) == EXIT_FAILURE) {
+      usage();
+    }
+  }
+  catch (Exception e) {
+    usage();
+  }
   return 0;
 }
